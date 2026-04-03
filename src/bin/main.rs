d822b5e10e3b96199df789a3c474dfd87e6e3029
Lorenzo Gennari <lrnzgnnr@gmail.com>
2026-04-03 18:32:46 +0200

    wifi

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -32,9 +32,9 @@ macro_rules! mk_static {
     }};
 }
 
-const SSID: &str = "Italia";
-const PASSWORD: &str = "Jagvetinte10";
-const MQTT_BROKER: (u8, u8, u8, u8) = (192, 168, 0, 108);
+const SSID: &str = "SKYWIFI_XR2RJ";
+const PASSWORD: &str = "WD2gINkeIiZN";
+const MQTT_BROKER: (u8, u8, u8, u8) = (192, 168, 0, 230);
 
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
